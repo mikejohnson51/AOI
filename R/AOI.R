2fd2b4429c3a6079e334166eb63f43b6f747408b
mikejohnson51 <mike.johnson@geog.ucsb.edu>
2018-07-01 23:55:22 -0600

    first additions (transfer from HydroData)

---
+++ R/AOI.R
@@ -0,0 +1,24 @@
+#' AOI
+#'
+#' \code{AOI} package
+#'
+#' Create Area of Interest (AOI) polygons and/or gridded rasters
+#'
+#' See the README on
+#'
+#' @docType package
+#' @name AOI
+#'
+#' @import sp
+#' @importFrom  sf as_Spatial st_geometry
+#' @importFrom  dplyr %>%
+#' @importFrom  dismo geocode
+#' @importFrom  stats setNames
+#' @importFrom  utils capture.output install.packages tail
+#' @importFrom  USAboundaries us_counties us_states
+#' @importFrom  rjson fromJSON
+#' @importFrom  raster rasterToPolygons
+
+
+
+NULL
