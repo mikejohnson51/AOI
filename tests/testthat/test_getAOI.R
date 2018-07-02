7fd7fb7c62789276b5826bd01bcf7516d46b33e2
mikejohnson51 <mike.johnson@geog.ucsb.edu>
2018-07-02 13:24:40 -0600

    add test cases

--- tests/testthat/test_getAOI.R
+++ tests/testthat/test_getAOI.R
@@ -1,10 +1,5 @@
 context("getAOI")
 
-state_only <- getAOI(state = "Colorado")
-multiple_states <- getAOI(state = c('CA', 'nevada'))
-state_county <- getAOI(state = 'California', county = c('Santa Barbara', 'ventura'))
-clip_1 <- getAOI(clip_unit = list('KMART near UCSB', 10, 10, 'lowerleft'))
-
 test_that("getAOI throws correct errors", {
   expect_error(getAOI(state = '23'), "State not recongized. Full names or abbreviations can be used. Please check spelling.")
   expect_error(getAOI(state= c('CA', 23)), "State not recongized. Full names or abbreviations can be used. Please check spelling.")
@@ -13,9 +8,22 @@ test_that("getAOI throws correct errors", {
   expect_error(getAOI(), "Requires a 'clip_unit' or 'state' parameter to execute")
 })
 
-test_that("AOI returns type 'S4'", {
-  expect_equal(typeof(state_only), "S4")
-  expect_equal(typeof(state_county), "S4")
-  expect_equal(typeof(clip_1), "S4")
-})
 
+test_that("check getAOI routines", {
+  one_state <- try(getAOI(state = "Colorado"))
+  sp_def <- try(getAOI(clip_unit = one_state))
+  two_state <- try(getAOI(state = c("AZ", "utah")))
+
+  one_county <- try(getAOI(state = 'TX', county = 'Harris'))
+  two_county <- try(getAOI(state = 'California', county = c('Santa Barbara', 'ventura')))
+
+  clip_3   <- try(getAOI(clip_unit = list('KMART near UCSB', 10, 10)))
+  clip_4_1 <- try(getAOI(clip_unit = list('University of Alabama', 10, 10, "upperleft")))
+  clip_4_2 <- try(getAOI(clip_unit = list(37, -119, 10, 10)))
+  clip_5   <- try(getAOI(clip_unit = list(35, -115, 10, 10, "lowerright")))
+
+  vec = c(one_state, sp_def, two_state, one_county, two_county, clip_3, clip_4_1, clip_4_2, clip_5)
+  print(!inherits(vec,"try-error"))
+  check = !inherits(vec,"try-error")
+  expect_true(check)
+})
