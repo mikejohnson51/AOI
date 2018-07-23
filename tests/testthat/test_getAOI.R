d5e60b6ef232276bb3767aea791072ed08258e56
Mike Johnson <mike.johnson@geog.ucsb.edu>
2018-07-22 18:02:21 -0600

    Update test_getAOI.R

--- tests/testthat/test_getAOI.R
+++ tests/testthat/test_getAOI.R
@@ -111,27 +111,27 @@ expect_true(check)
 
 test_that("check external routines", {
 
-  clip_counties_all <-  getAOI(state = "NY", county = "all")
-  state.bb          <-  getAOI(state = "CA", bb = TRUE)
-  clip_4            <-  getAOI(clip = list("UCSB", 10,10, "lowerleft"), sf = TRUE)
-  clip_sf           <-  getAOI(clip = list("UCSB", 10,10), sf = TRUE)
+  clip_counties_all <-  !is.null(getAOI(state = "NY", county = "all"))
+  state.bb          <-  !is.null(getAOI(state = "CA", bb = TRUE))
+  #clip_4            <-  getAOI(clip = list("UCSB", 10,10, "lowerleft"), sf = TRUE)
+  #clip_sf           <-  getAOI(clip = list("UCSB", 10,10), sf = TRUE)
   #clip_mi           <-  getAOI(clip = clip_sf)
   #clip_km           <-  getAOI(clip = list(35, -115, 10, 10), km = TRUE)
 
   vec = c(
 
-    is.null(clip_counties_all),
-    is.null(state.bb),
-    is.null(clip_4),
-    is.null(clip_sf)
+    clip_counties_all,
+    state.bb
+    #is.null(clip_4),
+    #is.null(clip_sf)
 
     #(clip_mi@bbox[1,1] != clip_km@bbox[1,1])
     )
 
   rm(clip_counties_all)
   rm(state.bb)
-  rm(clip_4)
-  rm(clip_sf)
+  #rm(clip_4)
+  #rm(clip_sf)
   #rm(clip_mi)
   #rm(clip_km)
 
